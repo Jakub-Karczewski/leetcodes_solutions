691fa720149135a6d030b1a16e8bab02c9801bf9
Jakub-Karczewski <66319795+Jakub-Karczewski@users.noreply.github.com>
2025-04-24 01:16:36 +0200

    Update Kth_largest_element_in_Array.py

--- Kth_largest_element_in_Array.py
+++ Kth_largest_element_in_Array.py
@@ -3,6 +3,7 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
         """
+        Sposób 1
         def partition(l, r, k, nums):
             piv_ind = r
             pivot = nums[piv_ind]
@@ -13,7 +14,7 @@ class Solution:
                 if nums[j] <= pivot:
                     nums[bord], nums[j] = nums[j], nums[bord]
                     bord += 1
-            nums[r], nums[bord] = nums[bord], nums[r]
+            nums[r], nums[bord] = nums[bord], nums[r]22
 
             if bord < k:
                 for i in range(bord, k+1):
@@ -41,7 +42,8 @@ class Solution:
         return nums[len(nums) - k]
         """
 
-        
+        """
+        Sposób 2
         my_heap = PriorityQueue()
         for i in range(k):
             my_heap.put(nums[i])
@@ -51,3 +53,28 @@ class Solution:
                 my_heap.get()
                 my_heap.put(nums[i])
         return my_heap.queue[0]
+        """
+
+        #Sposób 3
+        my_dict = dict()
+        my_heap = PriorityQueue()
+        for x in nums:
+            my_dict[x] = my_dict.get(x, 0) + 1
+        act_capacity = 1
+        vals = list(my_dict.items())
+        my_heap.put((vals[0][0], vals[0][1]))
+        for i in range (1, len(vals)):
+            print(my_heap.queue)
+            if act_capacity + vals[i][1] > k:
+                if vals[i][0] > my_heap.queue[0][0]:
+                    el, c = my_heap.get()
+                    act_capacity -= c
+                    while not my_heap.empty() and act_capacity - my_heap.queue[0][1] + vals[i][1] >= k and my_heap.queue[0][0] < vals[i][0]:
+                        x, y = my_heap.get()
+                        act_capacity -= y
+                    my_heap.put((vals[i][0], vals[i][1]))
+                    act_capacity += vals[i][1]
+            else:
+                my_heap.put((vals[i][0], vals[i][1]))
+                act_capacity += vals[i][1]
+        return my_heap.queue[0][0]
